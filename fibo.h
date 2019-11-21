2c72120da9b6db649d1ed0e33f342e90214802d5
Adam Rozenek <adam.rozenek@gmail.com>
2019-11-21 19:08:23 +0100

    Comments and some improvements.

--- fibo.h
+++ fibo.h
@@ -49,30 +49,74 @@ public:
 
     [[nodiscard]] size_t length() const;
 
+    /** Overload addition assignment
+     * Set value to sum of this and another Fibo number
+     * @param[in] fibo - another Fibo number
+     * @return Reference to current Fibo number
+     */
     Fibo &operator+=(const Fibo &fibo);
 
+    /** Overload "fi-bitwise AND" assignment
+     * Set value to "fi-bitwise AND" of this and another Fibo number
+     * @param[in] fibo - another Fibo number
+     * @return Reference to current Fibo number
+     */
     Fibo &operator&=(const Fibo &fibo);
 
+    /** Overload "fi-bitwise OR" assignment
+     * Set value to "fi-bitwise OR" of this and another Fibo number
+     * @param[in] fibo - another Fibo number
+     * @return Reference to current Fibo number
+     */
     Fibo &operator|=(const Fibo &fibo);
 
+    /** Overload "fi-bitwise XOR" assignment
+     * Set value to "fi-bitwise XOR" of this and another Fibo number
+     * @param[in] fibo - another Fibo number
+     * @return Reference to current Fibo number
+     */
     Fibo &operator^=(const Fibo &fibo);
 
+    /** Overload "fi-bitwise left shift" assignment
+     * Set value to "fi-bitwise left shift" of this and another Fibo number
+     * @param[in] n - number of places to shift
+     * @return Reference to current Fibo number
+     */
     Fibo &operator<<=(unsigned int n);
 
-    friend std::ostream &operator<<(std::ostream &os, const Fibo &fibo);
-
+    /** Less than Fibo numbers operator
+     * Checks if fibo1 < fibo2
+     * @param[in] fibo1 - Fibo number
+     * @param[in] fibo2 - Fibo number
+     * @return @p true - fibo1 < fibo2, @p false - otherwise
+     */
     friend bool operator<(const Fibo &fibo1, const Fibo &fibo2);
 
+    /** Equal Fibo numbers operator
+     * Checks if two Fibo numbers are equal
+     * @param[in] fibo1 - Fibo number
+     * @param[in] fibo2 - Fibo number
+     * @return @p true - fibo1 == fibo2, @p false - otherwise
+     */
     friend bool operator==(const Fibo &fibo1, const Fibo &fibo2);
 
+    /** Output operator for Fibo numbers
+     * Print Fibo number through stream
+     * @param[out] os - output stream
+     * @param[in] fibo - Fibo number to print
+     * @return Reference to output stream
+     */
+    friend std::ostream &operator<<(std::ostream &os, const Fibo &fibo);
 };
 
-bool operator<(const Fibo &fibo1, const Fibo &fibo2);
-
-bool operator==(const Fibo &fibo1, const Fibo &fibo2);
-
+/** Reference to immutable Fibo(0)
+ * @return Reference to const Fibo(0)
+ */
 const Fibo &Zero();
 
+/** Reference to immutable Fibo(1)
+ * @return Reference to const Fibo(1)
+ */
 const Fibo &One();
 
 #endif //FIBO_H
